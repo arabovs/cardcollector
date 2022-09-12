4844657ab50a4fcc5ccea41f20f79a45729906cb
arabovs <arabov.simeon1990@gmail.com>
2022-09-12 22:43:30 +0100

    fixing apollo client imports

---
+++ apps/fe/src/client/client.ts
@@ -0,0 +1,44 @@
+import React from "react";
+import { gql, useSubscription } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+  split,
+} from "@apollo/client";
+
+import fetch from "isomorphic-fetch";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
+import isNode from "is-node";
+import ws from "ws";
+
+const wsLink = new WebSocketLink({
+  uri: "wss://d2r-smart-chain.herokuapp.com/v1/graphql",
+  options: {
+    reconnect: true,
+  },
+  webSocketImpl: isNode ? ws : null,
+});
+
+const httpLink = createHttpLink({
+  uri: "https://d2r-smart-chain.herokuapp.com/v1/graphql",
+  fetch,
+});
+
+const link = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
+
+export const client = new ApolloClient({
+  link: link,
+  cache: new InMemoryCache(),
+});
