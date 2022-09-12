a190a2ed268028f1c58e6d6638689f6eba704371
arabovs <arabov.simeon1990@gmail.com>
2022-09-12 23:00:57 +0100

    another brill fix

---
+++ apps/fe/src/gatsby-theme-material-ui-top-layout/components/top-layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ThemeTopLayout from "gatsby-theme-material-ui-top-layout/src/components/top-layout";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "../../client/client";
+import { Container } from "@mui/material";
+
+function getLibrary(provider) {
+
+
+export default function TopLayout({ children, theme }) {
+  return (
+    <ThemeTopLayout theme={theme}>
+      <ApolloProvider client={client}>
+      </ApolloProvider>
+    </ThemeTopLayout>
+  );
+}
