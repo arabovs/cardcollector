03aba49c7fac87b9d33330dde81a9dbfc6a41487
arabovs <arabov.simeon1990@gmail.com>
2022-09-18 17:29:04 +0100

    search pricing for file input

--- apps/be/LotrTCG.py
+++ apps/be/LotrTCG.py
@@ -82,13 +82,14 @@ def createNewURL(edition, card_name_cleaned):
   return URL_PRICING + editions_dict[edition] + "/" + card_name_cleaned
 
 #insert row in db pricing table
-def runGQL(card_name, card_edition, card_price, card_price_foil, card_price_tng, source):
-    query = gql("""mutation MyMutation($card_name: String!, $card_edition: String!, $card_price: float8!, $card_price_foil: float8!, $card_price_tng: float8!, $source: String!) {
+def gqlInsertCard(card_name, card_edition, card_price, card_price_foil, card_price_tng, source, card_id):
+    query = gql("""mutation MyMutation($card_name: String!, $card_edition: String!, $card_price: float8!, $card_price_foil: float8!, $card_price_tng: float8!, $source: String!, $card_id: String!) {
       insert_lotr_all_cards_pricing(objects: {card_name: $card_name,
                                       card_edition: $card_edition,
                                       card_price: $card_price,
                                       card_price_foil: $card_price_foil, 
                                       card_price_tng: $card_price_tng,
+                                      card_id: $card_id,
                                       source: $source 
                                       }) {
         affected_rows
@@ -101,40 +102,73 @@ def runGQL(card_name, card_edition, card_price, card_price_foil, card_price_tng,
         "card_price": card_price,
         "card_price_foil": card_price_foil,
         "card_price_tng": card_price_tng,
+        "card_id": card_id,
         "source": source
     }
     result = client.execute(query, variable_values=params)
+    
+    #insert row in db pricing table
+def gqlFindCardbyName(card_name):
+    query = gql("""query MyQuery($card_name: String!) {
+      lotr_all_cards_pricing(where: {card_name: {_ilike: $card_name}}) {
+            id
+            card_name
+            card_price
+            card_price_foil
+            card_price_tng
+      }
+    }""")
+ 
+    params = {
+        "card_name": ("%"+card_name+"%"),
+    }
+    result = client.execute(query, variable_values=params)
+    return result
 
 
 # PROCESS START
 
 print("Process Start:", now.strftime("%d/%m/%Y %H:%M:%S"))
 
-cards_table = soup.find_all('table', class_='inline') 
+def scrapeLatestPricing():
+    cards_table = soup.find_all('table', class_='inline') 
+    for cards in cards_table:
+        rows = cards.find_all('tr')
+        for row in rows:
+            card_id = str(row.find('td').string)
+            card_name_cleaned = cleanCardName(card_name = row.find('td', class_= 'col1').string)       
+            card_id_regex_number = re.compile(r"^([^a-zA-Z]*)") #Monk code kepp
+            edition = re.search(card_id_regex_number, card_id).group(0)
+    
+            # skipping here as we need to handle promo cards better
+            if  "Title" in card_name_cleaned:
+              print("Skipping: " + card_name_cleaned)
+              continue
+            
+            URL_PRICE = createNewURL(edition, card_name_cleaned)
+            
+            card_price      = getPriceFromURL(URL_PRICE) 
+            card_price_foil = getPriceFromURL(URL_PRICE + "-foil") 
+            card_price_tng  = getPriceFromURL(URL_PRICE + "-tengwar")
+           
+              
+            print(f"Inserting Card Name: " + card_name_cleaned + " with regular price of: " + str(card_price) + " and foil price: " + str(card_price_foil) + " and tengwar price: " + str(card_price_tng))
+            gqlInsertCard(str(row.find('td', class_= 'col1').string).replace("•",""),editions_dict[edition].replace(" ","-"),card_price, card_price_foil, card_price_tng,  source,str(row.find('td').string))
 
-for cards in cards_table:
-    rows = cards.find_all('tr')
-    for row in rows:
-        card_id = str(row.find('td').string)
-        card_name_cleaned = cleanCardName(card_name = row.find('td', class_= 'col1').string)       
-        card_id_regex_number = re.compile(r"^([^a-zA-Z]*)") #Monk code kepp
-        edition = re.search(card_id_regex_number, card_id).group(0)
-
-        # skipping here as we need to handle promo cards better
-        if  "Title" in card_name_cleaned:
-          print("Skipping: " + card_name_cleaned)
-          continue
-        
-        URL_PRICE = createNewURL(edition, card_name_cleaned)
-        
-        card_price      = getPriceFromURL(URL_PRICE) 
-        card_price_foil = getPriceFromURL(URL_PRICE + "-foil") 
-        card_price_tng  = getPriceFromURL(URL_PRICE + "-tengwar")
-       
-          
-        print(f"Inserting Card Name: " + card_name_cleaned + " with regular price of: " + str(card_price) + " and foil price: " + str(card_price_foil) + " and tengwar price: " + str(card_price_tng))
-        runGQL(card_name_cleaned.lower().replace("-"," "),editions_dict[edition].replace(" ","-"),card_price, card_price_foil, card_price_tng,  source)
 
 
-# PROCESS END
+def printCardByName(card_name):
+    data = gqlFindCardbyName(card_name)
+    for array in data["lotr_all_cards_pricing"]:
+      print(array["card_price"])
+    
+
+def openFile(input_filename):
+  f = open(input_filename, "r")
+  for x in f:
+     printCardByName(x.replace("\n",""))
+
+#openFile("test/input.test")
+scrapeLatestPricing()
+  
 print("Process End:", now.strftime("%d/%m/%Y %H:%M:%S"))
