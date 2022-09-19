81a5309abeb878d897f6866b51d51a2f5d2d31db
arabovs <arabov.simeon1990@gmail.com>
2022-09-19 18:08:35 +0100

    add branch

---
+++ apps/fe/src/pages/search.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Grommet } from "../components/Grommet";
+
+const SearchPage = () => {
+  return (
+    <div>
+      <Grommet inputText="Frodo" />
+      <Grommet inputText="Frodo" />
+      <Grommet inputText="Frodo" />
+    </div>
+  );
+};
+
+export default SearchPage;
