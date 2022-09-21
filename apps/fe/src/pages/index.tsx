610ca17fcfb7c854efda5674d9ff140f54e9378e
arabovs <arabov.simeon1990@gmail.com>
2022-09-21 21:20:01 +0100

    add image

--- apps/fe/src/pages/index.tsx
+++ apps/fe/src/pages/index.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Link } from "gatsby";
 import { Box, Grid, TextField, Typography } from "@mui/material";
 import { PricingComponent } from "../components/PricingComponent";
+import Image from "material-ui-image";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
 
 const IndexPage = () => {
   const [inputText, setInputText] = React.useState<any | null>(null);
@@ -10,6 +13,11 @@ const IndexPage = () => {
     <div>
       {" "}
       <Link to="/search">Go to search</Link>
+      <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+        <ImageListItem>
+          <img src="https://lotrtcgwiki.com/wiki/_media/cards:lotr00001.jpg" />
+        </ImageListItem>
+      </ImageList>
       <Typography variant="h5" fontWeight={"bold"}>
         All posts:
       </Typography>
