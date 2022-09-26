06fd32237f6209c972ade0729a71098aac3bb50c
arabovs <arabov.simeon1990@gmail.com>
2022-09-26 21:45:23 +0100

    changing to MUI

--- apps/fe/src/pages/search.tsx
+++ apps/fe/src/pages/search.tsx
@@ -4,6 +4,10 @@ import { GrommetButton } from "../components/GrommetButton";
 import { MUIButton } from "../components/MUIButton";
 import { Box, Grid, TextField, Typography } from "@mui/material";
 import { SetComponent } from "../components/SetComponent";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 
 const SearchPage = () => {
   const [value, setValue] = React.useState("Promotional");
@@ -16,36 +20,52 @@ const SearchPage = () => {
     <>
       <Link to="/">Go to Main</Link>
       <div>
-        <label>
-          Card Set:
-          <select value={value} onChange={handleChange}>
-            <option value="Promotional">Promotional</option>
-            <option value="The-Fellowship-of-the-Ring">
-              The Fellowship of the Ring
-            </option>
-            <option value="Mines-of-Moria">Mines of Moria</option>
-            <option value="Realms-of-the-Elf-lords">
-              Realms of the Elf-lords
-            </option>
-            <option value="The-two-Towers">The two Towers</option>
-            <option value="Battle-of-helms-Deep">Battle of helm's Deep</option>
-            <option value="Ents-of-Fangorn">Ents of Fangorn</option>
-            <option value="The-Return-of-the-King">
-              The Return of the King
-            </option>
-            <option value="Siege-of-Gondor">Siege of Gondor</option>
-            <option value="Reflections">Reflections</option>
-            <option value="Mount-Doom">Mount Doom</option>
-            <option value="Shadows">Shadows</option>
-            <option value="Black-Rider">Black Rider</option>
-            <option value="Bloodlines">Bloodlines</option>
-            <option value="Expanded-Middle-earth">Expanded Middle-earth</option>
-            <option value="The-Hunters">The Hunters</option>
-            <option value="The-Wraith-Collection">The Wraith Collection</option>
-            <option value="Rise-of-Saruman">Rise of Saruman</option>
-            <option value="Treachery-and-Deceit">Treachery & Deceit</option>
-          </select>
-        </label>
+        <Box sx={{ minWidth: 120 }}>
+          <FormControl fullWidth>
+            Card Set:
+            <Select
+              labelId="edition-selector"
+              id="edition-selector"
+              value={value}
+              label="edition-selector"
+              onChange={handleChange}
+            >
+              <MenuItem value="Promotional">Promotional</MenuItem>
+              <MenuItem value="The-Fellowship-of-the-Ring">
+                The Fellowship of the Ring
+              </MenuItem>
+              <MenuItem value="Mines-of-Moria">Mines of Moria</MenuItem>
+              <MenuItem value="Realms-of-the-Elf-lords">
+                Realms of the Elf-lords
+              </MenuItem>
+              <MenuItem value="The-two-Towers">The two Towers</MenuItem>
+              <MenuItem value="Battle-of-helms-Deep">
+                Battle of Helm's Deep
+              </MenuItem>
+              <MenuItem value="Ents-of-Fangorn">Ents of Fangorn</MenuItem>
+              <MenuItem value="The-Return-of-the-King">
+                The Return of the King
+              </MenuItem>
+              <MenuItem value="Siege-of-Gondor">Siege of Gondor</MenuItem>
+              <MenuItem value="Reflections">Reflections</MenuItem>
+              <MenuItem value="Mount-Doom">Mount Doom</MenuItem>
+              <MenuItem value="Shadows">Shadows</MenuItem>
+              <MenuItem value="Black-Rider">Black Rider</MenuItem>
+              <MenuItem value="Bloodlines">Bloodlines</MenuItem>
+              <MenuItem value="Expanded-Middle-earth">
+                Expanded Middle-earth
+              </MenuItem>
+              <MenuItem value="The-Hunters">The Hunters</MenuItem>
+              <MenuItem value="The-Wraith-Collection">
+                The Wraith Collection
+              </MenuItem>
+              <MenuItem value="Rise-of-Saruman">Rise of Saruman</MenuItem>
+              <MenuItem value="Treachery-and-Deceit">
+                Treachery & Deceit
+              </MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
 
         <SetComponent inputText={value} />
       </div>
