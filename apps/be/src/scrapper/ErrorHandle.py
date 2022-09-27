af88d720fc94bda60ecbabcfb2473f151d7d7235
mitky <dimitar.shikrenov@gmail.com>
2022-09-27 18:25:01 +0100

    Erros handled to card 874

---
+++ apps/be/src/scrapper/ErrorHandle.py
@@ -0,0 +1,52 @@
+class ErrorHandle():
+
+  
+
+        def check_URL(page_url):
+
+                
+                # if page_url(last element) == "-tengward" replace with "-t" !!!!!
+
+    
+                card_back_img = "https://www.ccgcastle.com/product/lotr-tcg/lotr-complete-sets/mines-of-moria-complete-set" #for missing cards
+
+                if(page_url == "https://www.ccgcastle.com/product/lotr-tcg/lotr-promotional/Neekerbreekers--Bog-D"): # handle signel card cus of name
+                    page_url = page_url.replace("--","-")
+                if(page_url == "https://www.ccgcastle.com/product/lotr-tcg/The-Fellowship-of-the-Ring/The-One-Ring-Isildur-s-Bane-tengwar"):   
+                    page_url = card_back_img # handle isildur's bane
+                if(page_url == "https://www.ccgcastle.com/product/lotr-tcg/The-Fellowship-of-the-Ring/Cave-Troll-of-Moria-Scourge-of-the-Black-Pit-tengwar"):   
+                    page_url = page_url.replace("-tengwar","")  # handel Troll. Error in the site URL (tengwar exist but no link created by site creator)
+                if(page_url == "https://www.ccgcastle.com/product/lotr-tcg/The-Fellowship-of-the-Ring/Ulaire-Enquea-Lieutenant-of-Morgul-tengwar"):   
+                    page_url = page_url.replace("-tengwar","-t")
+                if(page_url == "https://www.ccgcastle.com/product/lotr-tcg/The-Fellowship-of-the-Ring/Ulaire-Otsea-Lieutenant-of-Morgul"):   
+                    page_url = card_back_img # non-exicting card
+                if(page_url == "https://www.ccgcastle.com/product/lotr-tcg/The-Fellowship-of-the-Ring/Bilbo-Retired-Adventurer"):   
+                    page_url = "https://www.ccgcastle.com/product/lotr-tcg/fellowship-of-the-ring/bilbo-baggins-retired-adventurer" # site URL error
+                if(page_url == "https://www.ccgcastle.com/product/lotr-tcg/Mines-of-Moria/Gloin-Friend-to-Thorin"):   
+                    page_url = card_back_img # non-exicting card
+                if(page_url == """https://www.ccgcastle.com/product/lotr-tcg/Mines-of-Moria/Speak-"Friend"-and-Enter"""):   
+                    page_url = "https://www.ccgcastle.com/product/lotr-tcg/Mines-of-Moria/Speak-Friend-and-Enter"
+                if(page_url == "https://www.ccgcastle.com/product/lotr-tcg/Mines-of-Moria/The-Balrog-Flame-of-Udun"):   
+                    page_url = card_back_img # non-exicting card
+                if(page_url == "https://www.ccgcastle.com/product/lotr-tcg/Mines-of-Moria/Gimli-Dwarf-of-the-Mountain-race-M"):   
+                    page_url = card_back_img # non-exicting card
+                if(page_url == "https://www.ccgcastle.com/product/lotr-tcg/Realms-of-the-Elf-lords/Mines-of-Khazad-Dum"):   
+                    page_url = card_back_img # non-exicting card
+                if(page_url == "https://www.ccgcastle.com/product/lotr-tcg/Realms-of-the-Elf-lords/Arwen-Lady-Undomiel"):   
+                    page_url = card_back_img # non-exicting card
+                if(page_url == "https://www.ccgcastle.com/product/lotr-tcg/Realms-of-the-Elf-lords/Forests-of-Lothlorien"):   
+                    page_url = card_back_img # non-exicting card
+                if(page_url == "https://www.ccgcastle.com/product/lotr-tcg/Realms-of-the-Elf-lords/The-Palantir-of-Orthanc"):   
+                    page_url = card_back_img # non-exicting card
+                if(page_url == "https://www.ccgcastle.com/product/lotr-tcg/Realms-of-the-Elf-lords/Tower-of-Barad-dur"):   
+                    page_url = card_back_img # non-exicting card
+                if(page_url == "https://www.ccgcastle.com/product/lotr-tcg/The-Two-Towers/Hides-tengwar"):   
+                    page_url = page_url.replace("-tengwar","-t")
+                if(page_url == "https://www.ccgcastle.com/product/lotr-tcg/The-Two-Towers/Gimli-Unbidden-Guest"):   
+                    page_url = "https://www.ccgcastle.com/product/lotr-tcg/The-Two-Towers/Gimli-Unbidden-Guest-foil"
+                if(page_url == "https://www.ccgcastle.com/product/lotr-tcg/The-Two-Towers/Legolas-Dauntless-Hunter-tengwar"):   
+                    page_url = page_url.replace("-tengwar","-t")
+
+
+                return page_url
+  
--- apps/be/src/scrapper/LotrTCG.py
+++ apps/be/src/scrapper/LotrTCG.py
@@ -4,11 +4,13 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 from GQL import GQL
 from etc import etc
+from ErrorHandle import ErrorHandle
 import re
 
 source = "ccgcastle"
 URL = "https://lotrtcgwiki.com/wiki/grand" 
 URL_PRICING = "https://www.ccgcastle.com/product/lotr-tcg/" 
+card_back_img = "https://www.ccgcastle.com/product/lotr-tcg/lotr-complete-sets/mines-of-moria-complete-set" #for missing cards
 
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
@@ -20,28 +22,48 @@ gql_connector = GQL()
 # src/etc.py
 editions_dict = etc().editions_dict
 
+#src/ErrorHandle.py
+error_handle = ErrorHandle()
+
 
 def getImageFromURL(page_url, card_id):
+
+  page_url = ErrorHandle.check_URL(page_url)
 
   image_url = requests.get(page_url)
   soup_img = BeautifulSoup(image_url.content, "html.parser")
   card_img = soup_img.find(id = "product-image")
   site_url = "https://www.ccgcastle.com"
 
+  print(page_url)
+
   try:
     img = card_img['src'] 
     img_url = site_url + img
+    
   except:
+    
+
     if page_url[-1] == 'D':
       img_url = getImageFromURL(page_url.replace("-D",""), card_id)
-      print(img_url)
+      #print(img_url)
+
+    if page_url[-1] == 'M':
+      img_url = getImageFromURL(page_url.replace("-M",""), card_id)
+      #print(img_url)  
+    
     if page_url[-1] == 'P':
       new_page_url = re.sub(r".$", card_id, page_url)
       img_url = getImageFromURL(new_page_url, card_id)
+
+    
+    
   return img_url
 
 #get price from url, 0 if no valid url
 def getPriceFromURL(page_url): 
+  page_url = ErrorHandle.check_URL(page_url)
+
   page_price_url = requests.get(page_url)
   soup_price = BeautifulSoup(page_price_url.content, "html.parser")
   card_price = soup_price.find(class_='item-price')
@@ -76,7 +98,7 @@ def scrapeLatestPricing():
     for cards in cards_table:
         rows = cards.find_all('tr')
         for row in rows:
-            if increment > 1 and increment < 140:
+            if increment > 874 and increment < 5000:
               card_id = str(row.find('td').string)
               card_name_cleaned = cleanCardName(card_name = row.find('td', class_= 'col1').string)       
               card_id_regex_number = re.compile(r"^([^a-zA-Z]*)") #Monk code kepp
@@ -96,8 +118,8 @@ def scrapeLatestPricing():
 
               print(URL_PRICE)
               print(card_image)
-              gql_connector.gqlInsertCard(str(row.find('td', class_= 'col1').string).replace("•",""),editions_dict[edition].replace(" ","-"),card_price, card_price_foil, card_price_tng,  source,str(row.find('td').string), str(card_image))
-              print(f"Inserting Card " + str(increment) + " Name: " + card_name_cleaned + " with regular price of: " + str(card_price) + " and foil price: " + str(card_price_foil) + " and tengwar price: " + str(card_price_tng))
+              #gql_connector.gqlInsertCard(str(row.find('td', class_= 'col1').string).replace("•",""),editions_dict[edition].replace(" ","-"),card_price, card_price_foil, card_price_tng,  source,str(row.find('td').string), str(card_image))
+              #print(f"Inserting Card " + str(increment) + " Name: " + card_name_cleaned + " with regular price of: " + str(card_price) + " and foil price: " + str(card_price_foil) + " and tengwar price: " + str(card_price_tng))
 
             increment += 1
 
