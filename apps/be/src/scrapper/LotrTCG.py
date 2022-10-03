e537a5309bd5ca270d49fbeb6d9ef427d060b893
arabovs <arabov.simeon1990@gmail.com>
2022-10-03 22:51:32 +0100

    good night world

--- apps/be/src/scrapper/LotrTCG.py
+++ apps/be/src/scrapper/LotrTCG.py
@@ -70,7 +70,6 @@ def createNewURL(edition, card_name_cleaned):
 
 #used to obtain other info for card
 def cardURLgenerator(card_edition, card_number):
-  print(len(card_edition))
   if len(card_edition) == 1:
     card_edition = concat("0",card_edition)
   if len(card_number) == 1:
@@ -78,7 +77,26 @@ def cardURLgenerator(card_edition, card_number):
   if len(card_number) == 2:
     card_number = concat("0",card_number)
   return concat(card_edition,card_number)
-  
+
+def fetchCardDetailsDict(card_url):
+  card_details = requests.get(card_url)
+  soup_card_details = BeautifulSoup(card_details.content, "html.parser")
+  dict = {};  key = 0;  value = 0
+  for card_detail_row in soup_card_details.find_all('tr'):
+# THIS NEEDS FIXING
+   try:
+     key = card_detail_row.find('td', class_='col0').a.string
+     value = card_detail_row.find('td', class_='col1').a.string
+     dict[str(key).lower().replace(" ","_")] = str(value).lower()
+   except:
+     try:
+       key = str(card_detail_row.find('td', class_='col0').a.string)
+     except:
+       print("")
+     value = str(card_detail_row.find('td', class_='col1')).replace("<td class=\"col1\"> ","").replace("</td>","")
+     dict[str(key).lower().replace(" ","_")] = str(value).lower()
+     
+  return dict
 # PROCESS START
 
 print("Process Start:", now.strftime("%d/%m/%Y %H:%M:%S"))
@@ -89,50 +107,31 @@ def scrapeLatestPricing():
     for cards in cards_table:
         rows = cards.find_all('tr')
         for row in rows:
-            if increment > 2 and increment < 200:
+            if increment > 200 and increment < 250:
               
               # Basic Card info from Grand Page
               card_id = str(row.find('td').string)
+              card_name = str(row.find('td', class_= 'col1').string).replace("•","")
               card_id_regex_number = re.compile(r"^([^a-zA-Z]*)\w+(\d+)") #Monk code kepp
               card_edition = re.search(card_id_regex_number, card_id).group(1)
               card_number = re.search(card_id_regex_number, card_id).group(2)
               card_name_cleaned = cleanCardName(card_name = row.find('td', class_= 'col1').string)     
-              card_type = str(row.find('td', class_= 'col2').find('a').string)
-              card_culture = str(row.find('td', class_= 'col3').find('a').string)
+              # DND card_type = str(row.find('td', class_= 'col2').find('a').string)
+              # DND card_culture = str(row.find('td', class_= 'col3').find('a').string)
               
               # Detailed Card Info from 
-              CARD_URL = "https://lotrtcgwiki.com/wiki/lotr" + cardURLgenerator(card_edition,card_number)
-              print(CARD_URL)
-              card_details = requests.get(CARD_URL)
-              soup_card_details = BeautifulSoup(card_details.content, "html.parser")
-              x = 0
-              dict = {}
-              key = 0
-              value = 0
-              for card_detail_row in soup_card_details.find_all('tr'):
-
-                try:
-                  key = card_detail_row.find('td', class_='col0').a.string
-                  value = card_detail_row.find('td', class_='col1').a.string
-                  dict[key] = value
-                except:
-                  try:
-                    key = str(card_detail_row.find('td', class_='col0').a.string)
-                  except:
-                    print("")
-                  value = str(card_detail_row.find('td', class_='col1')).replace("<td class=\"col1\"> ","").replace("</td>","")
-                  dict[key] = value
-                
-
-              print(dict)
-                
-                  
- 
-                #print(type(card_detail_row.find('td', class_='col1')))
-                
+              #print("https://lotrtcgwiki.com/wiki/lotr" + cardURLgenerator(card_edition,card_number))
+              card_dict = fetchCardDetailsDict("https://lotrtcgwiki.com/wiki/lotr" + cardURLgenerator(card_edition,card_number))
+              card_dict["card_name"] = card_name
+              card_dict["card_id"] = card_id
+              card_dict["card_edition"] = card_edition
+              card_dict["card_number"] = card_number
+              # DND card_dict["card_type"] = card_type.lower()
+              # DND card_dict["card_culture"] = card_culture.lower()
               
-              soup_card_details.find(class_='item-price')
-              
+              print(str(increment) + " " + str(card_dict))
+              #print(type(card_detail_row.find('td', class_='col1')))
+              #soup_card_details.find(class_='item-price')
               # skipping here as we need to handle promo cards better
               if  "Title" in card_name_cleaned:
                 print("Skipping: " + card_name_cleaned)
@@ -142,12 +141,13 @@ def scrapeLatestPricing():
               card_price_foil = getPriceFromURL(URL_PRICE + "-foil") 
               card_price_tng  = getPriceFromURL(URL_PRICE + "-tengwar")
               card_image      = getImageFromURL(URL_PRICE, card_id) 
-
-              print(URL_PRICE)
-              print(card_image)
+              print("HEY: " + card_dict["card_id"])
+              #print(URL_PRICE)
+              #print(card_image)
               #gql_connector.gqlInsertCard(str(row.find('td', class_= 'col1').string).replace("•",""),editions_dict[edition].replace(" ","-"),card_price, card_price_foil, card_price_tng,  source,str(row.find('td').string), str(card_image))
               #print(f"Inserting Card " + str(increment) + " Name: " + card_name_cleaned + " with regular price of: " + str(card_price) + " and foil price: " + str(card_price_foil) + " and tengwar price: " + str(card_price_tng))
-
+              #gqlInsertCard(self, card_name, card_edition, card_price, card_price_foil, card_price_tng, source, card_id, card_img,card_kind,card_culture,card_twilight,card_type,card_number):
+              gql_connector.gqlInsertCard( card_dict.get("card_name",""), card_dict.get("card_edition",""), 0, 0, 0, source, card_dict.get("card_id",""), "",card_dict.get("kind",""),card_dict.get("culture",""),card_dict.get("twilight",0),card_dict.get("card_type",""),card_dict.get("card_number","")) 
             increment += 1
 
 scrapeLatestPricing()
