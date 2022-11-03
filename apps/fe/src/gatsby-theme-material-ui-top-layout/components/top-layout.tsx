c7723149540fc3035cfbbea3e03224236a5a0ed2
kdichev <konstantin.dichev@gmail.com>
2022-11-03 16:16:39 +0100

    change to card_generic

--- apps/fe/src/gatsby-theme-material-ui-top-layout/components/top-layout.tsx
+++ apps/fe/src/gatsby-theme-material-ui-top-layout/components/top-layout.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { createContext, useContext, useState } from "react";
 import ThemeTopLayout from "gatsby-theme-material-ui-top-layout/src/components/top-layout";
-import { ApolloProvider } from "@apollo/client";
+import { ApolloProvider, useSubscription, gql } from "@apollo/client";
 import { client } from "../../client/client";
-import { AppBar, Toolbar, Typography, IconButton } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  MenuItem,
+  Select,
+} from "@mui/material";
 import { Box } from "@mui/system";
 import {
   AccountCircleOutlined,
@@ -10,25 +17,77 @@ import {
 } from "@mui/icons-material";
 import { Link } from "gatsby-theme-material-ui";
 
+const GAME_NAMES = {
+  lotr: "The Lord of the Rings",
+  mtg: "Magic: The Gathering",
+  pokemon: "Pokémon",
+  yugioh: "Yu-Gi-Oh!",
+};
+
+const GameSelectorContext = createContext(null);
+
+const GameSelectorProvider = ({ children }) => {
+  const [selectedGame, setSelectedGame] = useState("lotr");
+  return (
+    <GameSelectorContext.Provider value={{ selectedGame, setSelectedGame }}>
+      {children}
+    </GameSelectorContext.Provider>
+  );
+};
+export const useGameSelectorContext = () => {
+  const selected = useContext(GameSelectorContext);
+  return selected;
+};
+
+const GameSelector = () => {
+  const { data, loading, error } = useSubscription(gql`
+    subscription GamesQuery {
+      card_generic(distinct_on: tcg) {
+        tcg
+      }
+    }
+  `);
+  const { selectedGame, setSelectedGame } = useGameSelectorContext();
+  if (loading) return null;
+  if (error) return <div>{error.message}</div>;
+  return (
+    <>
+      <Select
+        value={selectedGame}
+        onChange={(e) => setSelectedGame(e.target.value)}
+        size="small"
+        sx={{ ml: 2 }}
+      >
+        {data.card_generic.map(({ tcg }) => (
+          <MenuItem value={tcg}>{GAME_NAMES[tcg]}</MenuItem>
+        ))}
+      </Select>
+    </>
+  );
+};
+
 export default function TopLayout({ children, theme }) {
   return (
     <ThemeTopLayout theme={theme}>
       <ApolloProvider client={client}>
-        <AppBar color="default" position="static" variant="outlined">
-          <Toolbar>
-            <Link to="/" color="inherit" sx={{ textDecoration: "none" }}>
-              <Typography variant="h5">Lotrmarket</Typography>
-            </Link>
-            <Box flex={1} />
-            <IconButton>
-              <AccountCircleOutlined />
-            </IconButton>
-            <IconButton>
-              <ShoppingCartOutlined />
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-        {children}
+        <GameSelectorProvider>
+          <AppBar color="default" position="static" variant="outlined">
+            <Toolbar>
+              <Link to="/" color="inherit" sx={{ textDecoration: "none" }}>
+                <Typography variant="h5">Lotrmarket</Typography>
+              </Link>
+              <GameSelector />
+              <Box flex={1} />
+              <IconButton>
+                <AccountCircleOutlined />
+              </IconButton>
+              <IconButton>
+                <ShoppingCartOutlined />
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+          {children}
+        </GameSelectorProvider>
       </ApolloProvider>
     </ThemeTopLayout>
   );
