bdcfb6a7037bdd691c63012b72074a3169d43ac7
arabovs <arabov.simeon1990@gmail.com>
2022-11-04 22:21:45 +0000

    backend changes only

--- apps/be/src/Hearthstone.py
+++ apps/be/src/Hearthstone.py
@@ -15,48 +15,61 @@ params = {
 print("getting access Token")
 response = requests.post(url, data=params)
 access_token = json.loads(response.text.replace("'", "\""))["access_token"]
-url_card = f"https://us.api.blizzard.com/hearthstone/cards?locale=en_US&access_token={access_token}"
+
+# gets 40 cards for some reason
+# url_card = f"https://us.api.blizzard.com/hearthstone/cards?locale=en_US&access_token={access_token}"
+
+# METADATA
+url_card = f"https://us.api.blizzard.com/hearthstone/metadata?locale=en_US&access_token={access_token}"
+# url_card = f"https://us.api.blizzard.com/hearthstone/metadata/sets?locale=en_US&access_token={access_token}"
+# url_card = f"https://us.api.blizzard.com/hearthstone/metadata/classes?locale=en_US&access_token={access_token}"
+# url_card = f"https://us.api.blizzard.com/hearthstone/metadata/keywords?locale=en_US&access_token={access_token}"
+
+
 
 cards = requests.get(url_card).json()
-
-for card in cards["cards"]:
-    
+print(cards)
+for card in cards: # for metadata
+#for card in cards["cards"]: # for all cards
+        print(card["id"],card["name"])
+        #print("ID "+ str(card["id"]) + " name: " + card["name"] +" : " + card["text"])
+        
         # clean up attack and health
-        attack_cleaned = None
-        if "attack" in card.keys():
-            attack_cleaned = float(card["attack"])
-            
-        health_cleaned = None
-        if "attack" in card.keys():
-            health_cleaned = float(card["health"])
+        #attack_cleaned = None
+        #if "attack" in card.keys():
+        #    attack_cleaned = float(card["attack"])
+        #    
+        #health_cleaned = None
+        #if "attack" in card.keys():
+        #    health_cleaned = float(card["health"])
 
-        # mana cost clean up
-        mana_cleaned = None
-        if "manaCost" in card.keys():
-            mana_cleaned = card["manaCost"]
-    
-        gql_connector.gqlInsertGenericCard(
-                "hearthstone",
-                str(card.get("slug","")),
-                card.get("name",""),
-                card.get("image",""),
-                str(card.get("cardTypeId",None)),
-                str(card.get("cardSetId",None)),
-                str(card.get("cardSetId",None)),
-                str(card.get("rarityId",None)),
-                None,
-                None,
-                None,
-                str(card.get("cardTypeId",None)),
-                None,
-                card.get("text",None),
-                card.get("flavorText",None),
-                mana_cleaned,
-                None,
-                attack_cleaned,
-                health_cleaned,
-                None
-            )
+        ## mana cost clean up
+        #mana_cleaned = None
+        #if "manaCost" in card.keys():
+        #    mana_cleaned = card["manaCost"]
+        
+        #gql_connector.gqlInsertGenericCard(
+        #        "hearthstone",
+        #        str(card.get("slug","")),
+        #        card.get("name",""),
+        #        card.get("image",""),
+        #        str(card.get("cardTypeId",None)),
+        #        str(card.get("cardSetId",None)),
+        #        str(card.get("cardSetId",None)),
+        #        str(card.get("rarityId",None)),
+        #        None,
+        #        None,
+        #        None,
+        #        str(card.get("cardTypeId",None)),
+        #        None,
+        #        card.get("text",None),
+        #        card.get("flavorText",None),
+        #        mana_cleaned,
+        #        None,
+        #        attack_cleaned,
+        #        health_cleaned,
+        #        None
+        #    )
 
 print("inserted")
 
