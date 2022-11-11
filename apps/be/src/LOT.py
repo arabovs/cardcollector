f0e005c41f79647aa0a87f6da58f23ea29539f33
arabovs <arabov.simeon1990@gmail.com>
2022-11-11 20:53:00 +0000

    pushec

---
+++ apps/be/src/LOT.py
@@ -0,0 +1,29 @@
+import json
+from hashlib import sha256
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode
+from os import environ
+
+
+hashed = sha256("bPF7i3iMBs6GrsSUaaHTRnFhn25ZCHvI".encode("utf8")).hexdigest()
+
+print(hashed)
+def record_participation():
+    req = Request(
+         method="POST",
+         url="https://scistarter.org/api/participation/hashed/"  + "?key=" + environ["SCISTARTER_API_KEY"],
+         data=urlencode(
+             {
+                 "hashed": hashed,
+                 "type": "classification",  # other options: 'collection', 'signup'
+                 "duration": 31,  # Seconds the user spent participating, or an estimate
+             }
+         ).encode("utf8"),
+     )
+
+
+    r = urlopen(req)
+    if r.status != 200:
+        raise Exception(r.status, r.reason)
+    return json.loads(r.read())
+
